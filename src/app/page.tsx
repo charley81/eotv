3bff56d89d302ad2f6a2dfa43b4433654318684e
ChrisKnowsDev <chrisharley81@gmail.com>
2024-04-30 17:19:19 -0400

    displaying temp data from db on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,17 @@
-import { Button } from '@/components/ui/button'
 import { db } from '@/server/db/index'
-
-type Post = {
-  id: number
-  title: string
-  content: string
-  createdAt: Date
-  updatedAt: Date | null
-}
 
 export const dynamic = 'force-dynamic'
 
 export default async function Home() {
   const events = await db.query.events.findMany()
-  console.log(events)
-  return <main className="p-16"></main>
+
+  return (
+    <main className="p-16">
+      {events.map((event) => (
+        <div key={event.id} className="p-4 ">
+          <h2 className="text-xl font-bold">{event.eventName}</h2>
+        </div>
+      ))}
+    </main>
+  )
 }
