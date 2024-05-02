28a28b09ee9e9c1172de8948d620da5941cb99c7
ChrisKnowsDev <chrisharley81@gmail.com>
2024-05-01 21:19:24 -0400

    fixed info text width issue on landing page

--- src/components/ui/landing/landing-info.tsx
+++ src/components/ui/landing/landing-info.tsx
@@ -1,14 +1,17 @@
+import Link from 'next/link'
 import { Button } from '../button'
 
 export default function LandingInfo() {
   return (
-    <div className="sm:text-center md:mt-10 xl:mt-0">
+    <div className="sm:text-center md:mt-10 xl:mt-0 xl:max-w-xl">
       <h1 className="text-4xl font-black">Events of The Villages</h1>
       <p className="text-base text-muted-foreground leading-8 mt-4">
         Your go-to neighborhood event app, seamlessly connecting locals with
         everything from block parties to backyard fires.
       </p>
-      <Button className="w-full mt-16 lg:mt-8">Get Started</Button>
+      <Button className="w-full mt-16 lg:mt-8" asChild>
+        <Link href="/all-events">Get Started</Link>
+      </Button>
     </div>
   )
 }
