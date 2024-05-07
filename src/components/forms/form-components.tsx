ddfec8bb30eb02c8f3bb2d6e0894bfe36535b621
ChrisKnowsDev <chrisharley81@gmail.com>
2024-05-07 18:13:19 -0400

    edited CustomDatePicker to allow selecting future dates

--- src/components/forms/form-components.tsx
+++ src/components/forms/form-components.tsx
@@ -69,7 +69,7 @@ export function CustomDatePicker({ name, control }: CustomDatePickerProps) {
                 selected={field.value}
                 onSelect={field.onChange}
                 disabled={(date) =>
-                  date > new Date() || date < new Date('1900-01-01')
+                  date < new Date() || date < new Date('1900-01-01')
                 }
                 initialFocus
               />
