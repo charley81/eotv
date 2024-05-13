dae1596105470c8b57a9fe836ef546ffa54836e7
ChrisKnowsDev <chrisharley81@gmail.com>
2024-05-13 14:21:25 -0400

    search form styles complete

--- src/components/forms/search-form.tsx
+++ src/components/forms/search-form.tsx
@@ -21,21 +21,27 @@ export default function SearchForm() {
     console.log(search, category)
   }
   return (
-    <form onSubmit={handleSubmit}>
-      <Input type="text" placeholder="Search Events" name="search" />
-      <Select name="category">
-        <SelectTrigger>
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent>
-          {['All', ...Object.values(Category)].map((category) => (
-            <SelectItem key={category} value={category}>
-              {category}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      <Button type="submit" className="w-full">
+    <form
+      onSubmit={handleSubmit}
+      className="mt-20 p-4 max-w-3xl mx-auto xl:flex xl:justify-between xl:items-center xl:gap-x-4 xl:max-w-6xl"
+    >
+      <section className="grid gap-y-5 sm:grid-cols-2 sm:gap-y-0 sm:gap-x-4 xl:flex xl:flex-1 xl">
+        <Input type="text" placeholder="Search Events" name="search" />
+        <Select name="category">
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            {['All', ...Object.values(Category)].map((category) => (
+              <SelectItem key={category} value={category}>
+                {category}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </section>
+
+      <Button type="submit" className="mt-8 w-full xl:mt-0 xl:w-1/3">
         Search
       </Button>
     </form>
